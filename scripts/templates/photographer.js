9bdc29bb0487501d67e1370bb395d27ce861ee1e
Kushchina Alena <alenakushchina@gmail.com>
2024-07-22 09:26:48 +0200

    Initial commit

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,33 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { id, name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `/assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `${name}'s portrait`);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const location = document.createElement( 'p' );
+        location.textContent = `${city}, ${country}`;
+
+        const tag = document.createElement( 'p' );
+        tag.textContent = tagline;
+
+        const cost = document.createElement( 'p' );
+        cost.textContent = `${price}€/jour`
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(tag);
+        article.appendChild(cost)
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { id, name, picture, city, country, tagline, price, getUserCardDOM }
 }
