234343cc1e6c210b11d5cd1024414991f8bc0f30
Kushchina Alena <alenakushchina@gmail.com>
2024-08-15 22:37:31 +0200

    correction de l accessibilité des lightbox à partir du clavier, des styles de lightbox et de la navigation

--- scripts/pages/media.js
+++ scripts/pages/media.js
@@ -17,91 +17,11 @@ function sortByTitle(a, b) {
     return a.title.localeCompare(b.title);
 }
 
-function createLightBoxContent() {
-    const lightBoxContent = document.querySelector('.lightBox_content');
-    const ul = document.createElement('ul');
-    ul.classList.add('media-list');
-
-    photographerMedia.forEach((mediaItem, index) => {
-        const li = document.createElement('li');
-        li.classList.add('media-item');
-        li.setAttribute('data-index', index);
-        li.style.display = 'none'; // Скрываем все элементы по умолчанию
-
-        const figure = document.createElement('figure');
-
-        if (mediaItem.image) {
-            const img = document.createElement('img');
-            img.setAttribute('src', `../../assets/media/${photographerFolder}/${mediaItem.image}`);
-            img.setAttribute('alt', mediaItem.title);
-            img.setAttribute('aria-label', mediaItem.title);
-            figure.appendChild(img);
-        } else if (mediaItem.video) {
-            const video = document.createElement('video');
-            video.setAttribute('src', `../../assets/media/${photographerFolder}/${mediaItem.video}`);
-            video.setAttribute('aria-label', mediaItem.title);
-            video.setAttribute('controls', true);
-            figure.appendChild(video);
-        }
-
-        const figcaption = document.createElement('figcaption');
-        figcaption.textContent = mediaItem.title;
-        figure.appendChild(figcaption);
-
-        li.appendChild(figure);
-        ul.appendChild(li);
-    });
-
-    lightBoxContent.innerHTML = ''; // Очищаем перед добавлением нового содержимого
-    lightBoxContent.appendChild(ul);
-}
-
-function updateLightBoxVisibility(index) {
-    const mediaItems = document.querySelectorAll('.media-list .media-item');
-    mediaItems.forEach(item => {
-        item.style.display = 'none'; // Скрыть все элементы
-    });
-    
-    const currentItem = document.querySelector(`.media-list .media-item[data-index="${index}"]`);
-    if (currentItem) {
-        currentItem.style.display = 'block'; // Показать текущий элемент
-    }
-}
-
-function setupNavigation() {
-    if (navigationInitialized) return;  // Проверяем, была ли навигация уже инициализирована
-
-    const navNext = document.getElementById('nav_next');
-    const navPrev = document.getElementById('nav_prev');
-
-    navNext.addEventListener('click', () => {
-        currentMediaIndex = (currentMediaIndex + 1) % photographerMedia.length;
-        console.log('Next index:', currentMediaIndex);
-        updateLightBoxVisibility(currentMediaIndex);
-    });
-
-    navPrev.addEventListener('click', () => {
-        currentMediaIndex = (currentMediaIndex - 1 + photographerMedia.length) % photographerMedia.length;
-        console.log('Previous index:', currentMediaIndex);
-        updateLightBoxVisibility(currentMediaIndex);
-    });
-
-    document.getElementById('lightBox').addEventListener('keydown', (event) => {
-        if (event.key === 'ArrowRight') {
-            navNext.click();
-        } else if (event.key === 'ArrowLeft') {
-            navPrev.click();
-        }
-    });
-
-    navigationInitialized = true;  // Помечаем навигацию как инициализированную
-}
-
 export async function getMedia(photographerId, sortBy = 'popularity') {
     const media = await fetchMedia();
     const photographer = await getPhotographerById(photographerId);
     const mediaGallery = document.getElementById('media-gallery');
-    mediaGallery.innerHTML = ''; // Очистить галерею перед добавлением медиа
+    mediaGallery.innerHTML = '';
     photographerMedia = media.filter(item => item.photographerId === photographerId);
     console.log(photographerMedia);
     photographerFolder = photographer.name;
@@ -200,7 +120,11 @@ function openLightBox() {
     lightBox.setAttribute('aria-hidden', 'false');
     document.getElementById('overlay').style.display = 'block';
 
-    setupNavigation();  // Инициализируем навигацию
+
+    const closeButton = document.getElementById('close_lightbox');
+    closeButton.focus();
+
+    setupNavigation();
 }
 
 window.closeLightBox = closeLightBox;
@@ -210,6 +134,130 @@ function closeLightBox() {
     lightBox.setAttribute('aria-hidden', 'true');
     document.getElementById('overlay').style.display = 'none';
 }
+
+
+function createLightBoxContent() {
+    const lightBoxContent = document.querySelector('.lightBox_content');
+    const ul = document.createElement('ul');
+    ul.classList.add('media_list');
+
+    photographerMedia.forEach((mediaItem, index) => {
+        const li = document.createElement('li');
+        li.classList.add('media_element');
+        li.setAttribute('data-index', index);
+        li.style.display = 'none';
+
+        const figure = document.createElement('figure');
+
+        if (mediaItem.image) {
+            const img = document.createElement('img');
+            img.setAttribute('src', `../../assets/media/${photographerFolder}/${mediaItem.image}`);
+            img.setAttribute('alt', mediaItem.title);
+            img.setAttribute('aria-label', mediaItem.title);
+            figure.appendChild(img);
+        } else if (mediaItem.video) {
+            const video = document.createElement('video');
+            video.setAttribute('src', `../../assets/media/${photographerFolder}/${mediaItem.video}`);
+            video.setAttribute('aria-label', mediaItem.title);
+            video.setAttribute('controls', true);
+            figure.appendChild(video);
+        }
+
+        const figcaption = document.createElement('figcaption');
+        figcaption.textContent = mediaItem.title;
+        figure.appendChild(figcaption);
+
+        li.appendChild(figure);
+        ul.appendChild(li);
+    });
+
+    lightBoxContent.innerHTML = '';
+    lightBoxContent.appendChild(ul);
+
+    
+    document.getElementById('lightBox').addEventListener('keydown', trapFocus);
+}
+
+function updateLightBoxVisibility(index) {
+    const mediaItems = document.querySelectorAll('.media_list .media_element');
+    mediaItems.forEach(item => {
+        item.style.display = 'none';
+    });
+    
+    const currentItem = document.querySelector(`.media_list .media_element[data-index="${index}"]`);
+    if (currentItem) {
+        currentItem.style.display = 'block';
+    }
+}
+
+function setupNavigation() {
+    if (navigationInitialized) return;  // Проверяем, была ли навигация уже инициализирована
+
+    const navNext = document.getElementById('nav_next');
+    const navPrev = document.getElementById('nav_prev');
+
+    navNext.addEventListener('click', () => {
+        currentMediaIndex = (currentMediaIndex + 1) % photographerMedia.length;
+        console.log('Next index:', currentMediaIndex);
+        updateLightBoxVisibility(currentMediaIndex);
+    });
+
+    navPrev.addEventListener('click', () => {
+        currentMediaIndex = (currentMediaIndex - 1 + photographerMedia.length) % photographerMedia.length;
+        console.log('Previous index:', currentMediaIndex);
+        updateLightBoxVisibility(currentMediaIndex);
+    });
+
+    navigationInitialized = true;  // Помечаем навигацию как инициализированную
+}
+
+document.querySelector('[aria-label="Close lightBox"]').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        closeLightBox();
+    }
+});
+
+
+function handleNavigationKey(event, direction) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        if (direction === 'next') {
+            document.getElementById('nav_next').click();
+        } else if (direction === 'prev') {
+            document.getElementById('nav_prev').click();
+        }
+    }
+}
+
+document.querySelector('[aria-label="Show previous image"]').addEventListener('keydown', function(event) {
+    handleNavigationKey(event, 'prev');
+});
+
+document.querySelector('[aria-label="Show next image"]').addEventListener('keydown', function(event) {
+    handleNavigationKey(event, 'next');
+});
+
+function trapFocus(event) {
+    const focusableElements = 'img';
+    const lightBox = document.getElementById('lightBox');
+    const focusableContent = lightBox.querySelectorAll(focusableElements);
+    const firstFocusableElement = focusableContent[0];
+    const lastFocusableElement = focusableContent[focusableContent.length - 1];
+
+    if (event.key === 'Tab') {
+        if (document.activeElement === lastFocusableElement) {
+            event.preventDefault();
+            firstFocusableElement.focus();
+        }
+    }
+
+    if (!lightBox.contains(document.activeElement)) {
+        event.preventDefault();
+        firstFocusableElement.focus();
+    }
+}
+
+document.getElementById('lightBox').addEventListener('keydown', trapFocus);
 
 const photographerId = getPhotographerIdFromUrl();
 const filter = document.getElementById('filter');
